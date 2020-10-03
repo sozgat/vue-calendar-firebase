f6faf7bd7aed2068cfe2182a6c583976ce25e86b
sozgat <said.ozgat@hotmail.com>
2020-10-04 00:14:47 +0300

    +Update,Edit and New Event methods added.

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -6,6 +6,14 @@
             flat
         >
           <v-btn
+              color="primary"
+              class="mr-4"
+              @click="dialog = true"
+              dark
+          >
+            New Event
+          </v-btn>
+          <v-btn
               outlined
               class="mr-4"
               color="grey darken-2"
@@ -73,6 +81,24 @@
           </v-menu>
         </v-toolbar>
       </v-sheet>
+      <!-- Add event dialog -->
+      <v-dialog v-model="dialog" max-width="500">
+        <v-card>
+          <v-container>
+            <v-form @submit.prevent="addEvent">
+              <v-text-field v-model="name" type="text" label="event name (required)"></v-text-field>
+              <v-text-field v-model="details" type="text" label="detail"></v-text-field>
+              <v-text-field v-model="start" type="date" label="start (required)"></v-text-field>
+              <v-text-field v-model="end" type="date" label="end (required)"></v-text-field>
+              <v-text-field v-model="color" type="color" label="color (click to open color menu)"></v-text-field>
+              <v-btn type="submit" color="primary" class="mr-4" @click.stop="dialog=false">
+                Create Event
+              </v-btn>
+            </v-form>
+          </v-container>
+        </v-card>
+      </v-dialog>
+
       <v-sheet height="600">
         <v-calendar
             ref="calendar"
@@ -101,20 +127,26 @@
                 :color="selectedEvent.color"
                 dark
             >
-              <v-btn icon>
-                <v-icon>mdi-pencil</v-icon>
+              <v-btn @click="deleteEvent(selectedEvent.id)" icon>
+                <v-icon>mdi-delete</v-icon>
               </v-btn>
               <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
               <v-spacer></v-spacer>
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-              <v-btn icon>
-                <v-icon>mdi-dots-vertical</v-icon>
-              </v-btn>
             </v-toolbar>
             <v-card-text>
-              <span v-html="selectedEvent.details"></span>
+              <form v-if="currentlyEditing !== selectedEvent.id">
+                {{selectedEvent.details}}
+              </form>
+              <form v-else>
+                <textarea-autosize
+
+                    v-model="selectedEvent.details"
+                    type="text"
+                    style="width: 100%;border-style: solid; border-radius: 2px; border-color: black;"
+                    :min-height="100"
+                    placeholder="add note"
+                ></textarea-autosize>
+              </form>
             </v-card-text>
             <v-card-actions>
               <v-btn
@@ -122,7 +154,21 @@
                   color="secondary"
                   @click="selectedOpen = false"
               >
-                Cancel
+                Close
+              </v-btn>
+              <v-btn
+                  text
+                  v-if="currentlyEditing !== selectedEvent.id"
+                  @click.prevent="editEvent(selectedEvent)"
+              >
+                Edit
+              </v-btn>
+              <v-btn
+                  text
+                  v-else
+                  @click.prevent="updateEvent(selectedEvent)"
+              >
+                Save
               </v-btn>
             </v-card-actions>
           </v-card>
@@ -171,6 +217,39 @@ export default {
       });
       this.events = events;
     },
+    async addEvent(){
+      if (this.name && this.start && this.end){
+        await db.collection('calEvent').add({
+          name: this.name,
+          details: this.details,
+          start: this.start,
+          end: this.end,
+          color: this.color
+        });
+        this.getEvents();
+        this.name="";
+        this.details="";
+        this.start="";
+        this.end="";
+        this.color="";
+      }
+      else{
+        alert('Name,start and end date are required!');
+      }
+
+    },
+    async updateEvent(ev){
+      await db.collection('calEvent').doc(this.currentlyEditing).update({
+        details:ev.details
+      });
+      this.selectedOpen = false;
+      this.currentlyEditing = null;
+    },
+    async deleteEvent(ev){
+      await db.collection('calEvent').doc(ev).delete();
+      this.selectedOpen = false;
+      this.getEvents();
+    },
     viewDay ({ date }) {
       this.focus = date
       this.type = 'day'
@@ -186,6 +265,9 @@ export default {
     },
     next () {
       this.$refs.calendar.next()
+    },
+    editEvent(ev){
+      this.currentlyEditing = ev.id;
     },
     showEvent ({ nativeEvent, event }) {
       const open = () => {
